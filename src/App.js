54570906886cbb9f7dd8831142c4cb242e5b3639
peterjohnson1000 <peterj1298@gmail.com>
2022-11-03 12:25:55 +0530

    ✅ fixed details not loading & added basic styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import Display from "./display";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [user, setUser] = useState("");
-  // const [button, setButton] = useState(false);
   const [userdata, setUserdata] = useState([]);
 
   const apicall = (e) => {
     e.preventDefault();
-    axios.get(`https://api.github.com/users/${user}`).then((res) => {
-      // console.log(res.data);
-      setUserdata(res.data);
-    });
+    axios
+      .get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        // console.log(res.data);
+        setUserdata(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("no user found");
+      });
   };
 
-  // console.log(Object.keys(userdata));
+  console.log(Object.keys(userdata));
+  console.log(userdata);
 
   return (
-    <div className="App">
-      <form>
+    <div className="h-screen bg-[#121212] text-[#C4C5C5] p-5">
+      <form className="text-center">
         <input
-          className="border-2 m-2"
-          placeholder="Enter username"
+          className="border bg-[#1D1D1D] p-3 rounded-full"
+          placeholder="username"
           value={user}
           onChange={(e) => {
             setUser(e.target.value);
           }}
         />
-        <button onClick={apicall}>Enter</button>
+        <button className="p-2 rounded-md bg-[#C4C5C5] ml-2" onClick={apicall}>
+          🔍
+        </button>
       </form>
       {Object.keys(userdata).length > 0 ? (
         <Display
           image={userdata.avatar_url}
-          Username={userdata.login}
-          Location={userdata.location}
+          name={userdata.name}
+          username={userdata.login}
+          location={userdata.location}
           github={userdata.created_at.slice(0, 10)}
         />
       ) : (
